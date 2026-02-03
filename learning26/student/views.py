0c37066124ff42ba067f1e119240bb4908420899
Devpatel-11-ai <devdevpatelpatel123@gmail.com>
2026-02-03 22:38:14 +0530

    student id and marks app created 03 02 26

--- learning26/student/views.py
+++ learning26/student/views.py
@@ -9,3 +9,11 @@ def studentDashboard(request):
     return render(request,"student/studentDashboard.html",student)    
     #student/studentDashboard.html
     #folder/filename
+
+def studentmarks(request):
+    marks = {"Yaksh":99,"Jeel":95,"Om":97}
+    return render(request,"student/studentmarks.html",marks)    
+
+def studentid(request):
+    id = {"Yaksh":101,"Jeel":102,"Om":103}
+    return render(request,"student/studentid.html",id)    
