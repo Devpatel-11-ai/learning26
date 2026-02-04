9d9e354d5ad2a230a45a7e57add24629c9c4bcb0
Devpatel-11-ai <devdevpatelpatel123@gmail.com>
2026-02-04 14:16:13 +0530

    student and product model created 04 02 26

--- learning26/learning26/settings.py
+++ learning26/learning26/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student'
+
+    #'django.contrib.student',
+    #'django.contrib.product',
 ]
 
 MIDDLEWARE = [
