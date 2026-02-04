817c985cc3d85a78233a88e65871a07b4b3b1908
Devpatel-11-ai <devdevpatelpatel123@gmail.com>
2026-02-04 18:57:00 +0530

    04 02 26

--- learning26/student/models.py
+++ learning26/student/models.py
@@ -10,7 +10,8 @@ class Student(models.Model):
     studentName= models.CharField(max_length=100)
     studentAge = models.IntegerField()
     studentCity = models.CharField(max_length=40)
-    #studentEmail = models.EmailField(null=True)
+    studentEmail = models.EmailField(null=True)
+    studentactivate = models.BooleanField(default=True)
 
     #meta class
     class Meta:
@@ -26,3 +27,14 @@ class Product(models.Model):
     
     class Meta:
         db_table = "product"
+
+class Iteams(models.Model):
+    iteamName = models.CharField(max_length=100)
+    iteamPrice = models.IntegerField()
+    iteamDescription = models.TextField()
+    iteamStock = models.PositiveIntegerField()
+    iteamColor = models.CharField(max_length=20,null=True)
+    iteamStatus = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "iteams"
