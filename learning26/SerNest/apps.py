3be2adff293d3a2d6165f6eb8c61d77d11229909
Devpatel-11-ai <devdevpatelpatel123@gmail.com>
2026-02-20 19:14:07 +0530

    some changes in SerNest file

--- learning26/SerNest/apps.py
+++ learning26/SerNest/apps.py
@@ -1,5 +1,5 @@
-# from django.apps import AppConfig
+from django.apps import AppConfig
 
 
-# class SernestConfig(AppConfig):
-#     name = 'SerNest'
+class SernestConfig(AppConfig):
+    name = 'SerNest'
